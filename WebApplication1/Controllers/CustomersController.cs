f32611ea74e3a493cab1b52b08ece4849a1e53f4
panicoenlaxbox <panicoenlaxbox@gmail.com>
2015-03-31 12:46:01 +0200

    Web API

---
+++ WebApplication1/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClassLibrary1;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        public IEnumerable<Customer> Get()
+        {
+            return new List<Customer>()
+            {
+                new Customer("1", "Customer 1"),
+                new Customer("1", "Customer 2")
+            };
+        }
+    }
+
+    public class CustomersRiskController : ApiController
+    {
+        public IEnumerable<CustomerRisk> Get()
+        {
+            return new List<CustomerRisk>()
+            {
+                new CustomerRisk("1", "Customer 1", 100),
+                new CustomerRisk("1", "Customer 2", 1500)
+            };
+        }
+    }
+}
